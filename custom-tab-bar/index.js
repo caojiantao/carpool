211ff2069b72a7e64a2913d6a9c70e4dd80870bc
caojiantao <caojiantao@zhuanzhuan.com>
2023-12-19 21:10:28 +0800

    完善模块样式

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,33 +1,35 @@
 Component({
-	data: {
-		active: 0,
-		list: [
-			{
-				icon: 'home-o',
-				text: '示例1',
-				url: '/pages/home/home'
-			},
-			{
-				icon: 'search',
-				text: '示例2',
-				url: '/pages/mine/mine'
-			}
-		]
-	},
+    data: {
+        active: 0,
+        list: [{
+                icon: 'home-o',
+                text: '首页',
+                url: '/pages/home/home'
+            },
+            {
+                icon: 'contact-o',
+                text: '我的',
+                url: '/pages/mine/mine'
+            }
+        ]
+    },
 
-	methods: {
-		onChange(event) {
-			this.setData({ active: event.detail });
-			wx.switchTab({
-				url: this.data.list[event.detail].url
-			});
-		},
+    methods: {
+        onChange(event) {
+            this.setData({
+                active: event.detail
+            });
+            wx.switchTab({
+                url: this.data.list[event.detail].url
+            });
+        },
 
-		init() {
-			const page = getCurrentPages().pop();
-			this.setData({
-				active: this.data.list.findIndex(item => item.url === `/${page.route}`)
-			});
-		}
-	}
+        init() {
+            const page = getCurrentPages().pop();
+            console.log(page);
+            this.setData({
+                active: this.data.list.findIndex(item => item.url === `/${page.route}`)
+            });
+        }
+    }
 });
--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -29,7 +29,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    this.getTabBar().init();
   },
 
   /**
